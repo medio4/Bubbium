756c98937a0f2160735fe4529f7c9c86f267c23e
Jedio <jgc412@gmail.com>
2020-07-08 11:18:36 -0400

    added //todo and //fixme list comments

--- src/main/java/jedio/spacemod/SpaceMod.java
+++ src/main/java/jedio/spacemod/SpaceMod.java
@@ -9,6 +9,17 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
+        //todo
+        // add these tools:
+        // - pickaxe
+        // - axe
+        // - shovel
+        // - hoe
+
+        //fixme
+        //
+
+
 @Mod("spacemod")
 public class SpaceMod {
 
