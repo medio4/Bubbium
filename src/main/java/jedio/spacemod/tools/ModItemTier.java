b52a68d488220125a53ef01fb6feb20bbb0772dd
Jedio <jgc412@gmail.com>
2020-07-08 01:10:05 -0400

    created Bubbium Item Tier with ModItemTier.java

---
+++ src/main/java/jedio/spacemod/tools/ModItemTier.java
@@ -0,0 +1,60 @@
+package jedio.spacemod.tools;
+
+import jedio.spacemod.util.RegistryHandler;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum ModItemTier implements IItemTier {
+    
+    BUBBIUM(3, 1087, 8.0f, 5, 14, () -> {
+            return Ingredient.fromItems(RegistryHandler.BUBBIUM_SWORD.get());
+    });
+    
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairMaterial;
+    
+    ModItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairMaterial = repairMaterial;
+    }
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return repairMaterial.get();
+    }
+}
