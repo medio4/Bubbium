3c9ef91b68dc5118580bfe56817cfc07212fd2c0
medio4 <43814257+medio4@users.noreply.github.com>
2020-07-20 02:26:27 -0400

    [from ExtraGolems]: added class that fixes a broken goal for the iron golem

---
+++ src/main/java/jedio/bubbium/entities/ai/MoveThroughVillageGoalFixed.java
@@ -0,0 +1,27 @@
+package jedio.bubbium.entities.ai;
+
+import net.minecraft.entity.CreatureEntity;
+import net.minecraft.entity.ai.goal.MoveThroughVillageGoal;
+import net.minecraft.pathfinding.GroundPathNavigator;
+
+import java.util.function.BooleanSupplier;
+
+public class MoveThroughVillageGoalFixed extends MoveThroughVillageGoal {
+
+  public MoveThroughVillageGoalFixed(final CreatureEntity entity, final double speedIn,
+                                     final boolean nocturnal, final int maxDistanceIn, final BooleanSupplier booleanSupplierIn) {
+    super(entity, speedIn, nocturnal, maxDistanceIn, booleanSupplierIn);
+  }
+  
+  @Override
+  public boolean shouldExecute() {
+    // The original implementation does an unsafe cast,
+    // so our implementation checks that the cast will be possible
+    // before allowing the task to continue
+    if(this.entity.getNavigator() instanceof GroundPathNavigator) {
+      return super.shouldExecute();
+    }
+    return false;
+  }
+
+}
